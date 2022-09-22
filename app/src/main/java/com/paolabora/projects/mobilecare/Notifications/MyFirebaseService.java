167f1cc7d262ac67682bab09e9dc7e646bc277a3
Paola Bashige <paolabashige@gmail.com>
2022-09-22 14:41:21 +0300

    Commenting MyFirebaseService under Notifications

--- app/src/main/java/com/paolabora/projects/mobilecare/Notifications/MyFirebaseService.java
+++ app/src/main/java/com/paolabora/projects/mobilecare/Notifications/MyFirebaseService.java
@@ -1,31 +1,31 @@
-package com.paolabora.projects.mobilecare.Notifications;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.FirebaseInstanceIdService;
-
-public class MyFirebaseService extends FirebaseInstanceIdService {
-
-    @Override
-    public void onTokenRefresh() {
-        super.onTokenRefresh();
-        FirebaseUser mUser = FirebaseAuth.getInstance().getCurrentUser();
-
-        String refreshToken = FirebaseInstanceId.getInstance().getToken();
-
-        if (mUser != null) {
-            updateToken(refreshToken);
-        }
-    }
-
-    private void updateToken(String refreshToken) {
-        FirebaseUser mUser = FirebaseAuth.getInstance().getCurrentUser();
-
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Tokens");
-        Token token = new Token(refreshToken);
-        reference.child(mUser.getUid()).setValue(token);
-    }
-}
+//package com.paolabora.projects.mobilecare.Notifications;
+//
+//import com.google.firebase.auth.FirebaseAuth;
+//import com.google.firebase.auth.FirebaseUser;
+//import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.database.FirebaseDatabase;
+//import com.google.firebase.iid.FirebaseInstanceId;
+//import com.google.firebase.iid.FirebaseInstanceIdService;
+//
+//public class MyFirebaseService extends FirebaseInstanceIdService {
+//
+//    @Override
+//    public void onTokenRefresh() {
+//        super.onTokenRefresh();
+//        FirebaseUser mUser = FirebaseAuth.getInstance().getCurrentUser();
+//
+//        String refreshToken = FirebaseInstanceId.getInstance().getToken();
+//
+//        if (mUser != null) {
+//            updateToken(refreshToken);
+//        }
+//    }
+//
+//    private void updateToken(String refreshToken) {
+//        FirebaseUser mUser = FirebaseAuth.getInstance().getCurrentUser();
+//
+//        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Tokens");
+//        Token token = new Token(refreshToken);
+//        reference.child(mUser.getUid()).setValue(token);
+//    }
+//}
